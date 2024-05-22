ab9536a495a1a182e655a8c4f5cffe166f5145c1
zagola <zagola@github.com>
2024-05-22 14:33:36 +0200

    Add quiz.js
    Add possibility to select answer.
    Add possibility to check answer.

---
+++ static/quiz.js
@@ -0,0 +1,45 @@
+var current_answer = "hehe";
+
+var check_answer = function(data) {
+    if(current_answer==data["correct"]) {
+        $('#infobox').html("Poprawna odpowiedź!");
+    } else {
+        $('#infobox').html("Zła odpowiedź!!");
+    }
+
+    console.log(data);
+    console.log(current_answer);
+};
+
+var on_check_click = function() {
+    $.getJSON("correct/1", check_answer);
+
+};
+
+var fetch_question = function(data) {
+    console.log(data);
+    $("#question").html(data["question"]);
+    $("#ansA").html(data["ansA"]);
+    $("#ansB").html(data["ansB"]);
+    $("#ansC").html(data["ansC"]);
+    $("#ansD").html(data["ansD"]);
+};
+
+$.getJSON("question/1", fetch_question);
+$(document).on('click', '#check', on_check_click);
+
+var on_answer_click = function() {
+    $("#ansA").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
+    $("#ansB").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
+    $("#ansC").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
+    $("#ansD").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
+
+    $(this).css("background-color", "lightgreen");
+
+    current_answer = $(this).html();
+};
+
+$(document).on('click', '#ansA', on_answer_click);
+$(document).on('click', '#ansB', on_answer_click);
+$(document).on('click', '#ansC', on_answer_click);
+$(document).on('click', '#ansD', on_answer_click);
