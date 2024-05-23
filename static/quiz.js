9481ca778c5425516ce8ddf80fcc1147ac5c6226
zagola <zagola@github.com>
2024-05-23 13:31:10 +0200

    Improve game flow
    Change next button visibility depending on game stage.
    Add message for user at the end of the game.

--- static/quiz.js
+++ static/quiz.js
@@ -1,10 +1,17 @@
 var current_answer = "hehe";
+var questionCounter = 1;
+var lastQuestion = 4;
 
 var check_answer = function(data) {
     if(current_answer==data["correct"]) {
         $('#infobox').html("Poprawna odpowiedź!");
+        $("#next").attr('disabled', false);
+        if(questionCounter >= lastQuestion) {
+             $('#infobox').html("Gratulacje!");
+        }
     } else {
         $('#infobox').html("Zła odpowiedź!!");
+        $("#next").attr('disabled', true);
     }
 
     console.log(data);
@@ -12,8 +19,7 @@ var check_answer = function(data) {
 };
 
 var on_check_click = function() {
-    $.getJSON("correct/1", check_answer);
-
+    $.getJSON("correct/" + questionCounter, check_answer);
 };
 
 var fetch_question = function(data) {
@@ -28,12 +34,15 @@ var fetch_question = function(data) {
 $.getJSON("question/1", fetch_question);
 $(document).on('click', '#check', on_check_click);
 
-var on_answer_click = function() {
+var reset_color= function() {
     $("#ansA").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
     $("#ansB").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
     $("#ansC").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
     $("#ansD").css("background-color", "hsl(var(--bulma-notification-h),var(--bulma-notification-s),var(--bulma-notification-background-l))");
+}
 
+var on_answer_click = function() {
+    reset_color();
     $(this).css("background-color", "lightgreen");
 
     console.log($(this).attr("id"));
@@ -44,3 +53,17 @@ $(document).on('click', '#ansA', on_answer_click);
 $(document).on('click', '#ansB', on_answer_click);
 $(document).on('click', '#ansC', on_answer_click);
 $(document).on('click', '#ansD', on_answer_click);
+
+var next_question = function() {
+    $('#infobox').html("Zaznacz poprawna odpowiedź.");
+    $("#next").attr('disabled', true);
+    questionCounter = questionCounter + 1;
+    $.getJSON("question/" + questionCounter, fetch_question);
+    reset_color();
+
+    if (questionCounter >= lastQuestion) {
+        $("#next").css('display', 'none');
+    }
+}
+
+$(document).on('click', '#next', next_question);
